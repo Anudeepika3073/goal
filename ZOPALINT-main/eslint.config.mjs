aef9a14fd1196762faa5181b2cb4b410d957931e
A.Anudeepika <100946437+Anudeepika3073@users.noreply.github.com>
2026-05-11 15:46:18 +0530

    Update eslint.config.mjs

--- ZOPALINT-main/eslint.config.mjs
+++ ZOPALINT-main/eslint.config.mjs
@@ -2,4 +2,13 @@ import fioriTools from '@sap-ux/eslint-plugin-fiori-tools';
 
 export default [
     ...fioriTools.configs.recommended
+    {
+        rules: {
+            "no-var": "error",
+            "semi": ["error", "always"],
+            "no-console": "warn",
+            "eqeqeq": "error",
+            "camelcase": "warn",
+            "no-unused-vars": "warn"
+        }
 ];
